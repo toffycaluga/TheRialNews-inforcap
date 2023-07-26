334976bb439b038a638e6dcf8bf45f04ecc271ae
Toffy Caluga <abraham.lillol@gmail.com>
2023-07-26 16:23:13 -0300

    FEAT:Eliminando controller article

--- app/controllers/articles_controller.rb
+++
@@ -1,9 +0,0 @@
-class ArticlesController < ApplicationController
-  load_and_authorize_resource
-  rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url, alert: "Acceso denegado."
-  end
-
-  def index
-  end
-end
