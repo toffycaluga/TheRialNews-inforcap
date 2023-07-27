4dee4ec67cd117e52644ff8149b158ce964e512e
Toffy Caluga <abraham.lillol@gmail.com>
2023-07-27 14:20:27 -0300

    FEAT:Test con errores , solucionar Error: ArticlesControllerTest#test_should_get_index: RuntimeError: Foreign key violations found in your fixture data. Ensure you aren't referring to labels that don't exist on associations.

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,13 +1,19 @@
 require "test_helper"
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @article = articles(:one)
+  include Devise::Test::IntegrationHelpers
+  setup do 
+    @article = articles(:first_article)
   end
 
   test "should get index" do
+
     get articles_url
     assert_response :success
+    # assert_equal 200, response.status
+    # assert_equal "text/html", response.media_type
+    # assert_equal "utf-8", response.charset
+    # assert_equal Article.all
   end
 
   test "should get new" do
@@ -16,8 +22,9 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should create article" do
+    sign_in users(:user_one)
     assert_difference("Article.count") do
-      post articles_url, params: { article: { description: @article.description, title: @article.title } }
+      post articles_url, params: { article: { description: @article.description, title: @article.title, user: @article.user } }
     end
 
     assert_redirected_to article_url(Article.last)
